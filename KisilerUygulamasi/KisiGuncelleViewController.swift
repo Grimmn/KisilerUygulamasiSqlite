08c899901199df619f596d7af697772cc93142a5
Utku Enes Alagöz <93991523+Grimmn@users.noreply.github.com>
2022-10-11 10:41:12 +0300

    Update KisiGuncelleViewController.swift

--- KisilerUygulamasi/KisiGuncelleViewController.swift
+++ KisilerUygulamasi/KisiGuncelleViewController.swift
@@ -1,10 +1,4 @@
-//
-//  KisiGuncelleViewController.swift
-//  KisilerUygulamasi
-//
-//  Created by Kasım Adalan on 29.07.2019.
-//  Copyright © 2019 info. All rights reserved.
-//
+
 
 import UIKit
 
